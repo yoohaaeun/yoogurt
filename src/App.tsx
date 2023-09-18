1e45a6f4ed88931e07d7c00af723838522b6403e
yoohaaeun <101792909+yoohaaeun@users.noreply.github.com>
2023-09-18 22:58:27 +0900

    라우팅 및 상태 관리 구조 개선

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
-import Movie from './pages/Movie';
-import NotFound from './pages/NotFound';
-import Root from './pages/Root';
-import Search from './pages/Search';
-import TvSeries from './pages/TvSeries';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: '/home/:contentId', element: <Home /> },
-      { path: '/tvSeries', element: <TvSeries /> },
-      { path: '/tvSeries/:contentId', element: <TvSeries /> },
-      { path: '/movie', element: <Movie /> },
-      { path: '/movie/:contentId', element: <Movie /> },
-      { path: '/search', element: <Search /> },
-      { path: '/search/:contentId', element: <Search /> },
-    ],
-  },
-]);
+import { Outlet } from 'react-router-dom';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,33 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import GlobalStyle from './GlobalStyle';
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+import NotFound from './pages/NotFound';
+import Search from './pages/Search';
+import TvSeries from './pages/TvSeries';
 import { theme } from './theme';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/home/:contentId', element: <Home /> },
+      { path: '/tvSeries', element: <TvSeries /> },
+      { path: '/tvSeries/:contentId', element: <TvSeries /> },
+      { path: '/movie', element: <Movie /> },
+      { path: '/movie/:contentId', element: <Movie /> },
+      { path: '/search', element: <Search /> },
+      { path: '/search/:contentId', element: <Search /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,7 +39,7 @@ root.render(
   <QueryClientProvider client={queryClient}>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </QueryClientProvider>
 );
